45e4149b4f9eb734dc4c71c79d758d91f9b267c1
Gautam Raj <gautamraj996@gmail.com>
2024-01-19 12:45:04 +0530

    02_frontend-backend_connection backend done

---
+++ 02_frontend_backend_connection/backend/index.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+const app = express()
+
+const PORT = process.env.PORT || 4000
+
+app.get('/jokes',(req,res)=>{
+    const jokes = [
+        {
+            id:1,
+            title:'A joke',
+            content:'This is a joke'
+        },
+        {
+            id:2,
+            title:'Another joke',
+            content:'This is another joke'
+        },
+        {
+            id:3,
+            title:'A third joke',
+            content:'This is third joke'
+        }
+    ]
+    res.send(jokes)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Serving at ${PORT}`)
+})
