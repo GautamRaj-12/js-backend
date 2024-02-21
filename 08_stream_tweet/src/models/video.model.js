97276d73e01e45e2a6424148e5f92748654aa14d
Gautam Raj <gautamraj996@gmail.com>
2024-02-21 09:21:54 +0530

    stream tweet mongoose aggregate paginate plugin added in video schema

--- 08_stream_tweet/src/models/video.model.js
+++ 08_stream_tweet/src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import aggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new mongoose.Schema(
   {
@@ -38,4 +39,5 @@ const videoSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+videoSchema.plugin(aggregatePaginate);
 export const Video = mongoose.model("Video", videoSchema);
